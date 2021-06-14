2f0c3399b61ef9cae77bf57c6a876f3a6f1683ee
Adarsh <aaa.narayanan@gmail.com>
2021-06-14 19:19:52 +0530

    jobs files updated

--- vue-jobs/src/router/index.js
+++ vue-jobs/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
+import Error from '../views/Error.vue'
+import Jobs from '../views/Jobs/Jobs.vue'
+import JobsDetails from '../views/Jobs/JobDetails.vue'
+
 
 const routes = [
   {
@@ -12,6 +16,28 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
+  },
+   {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobsDetails,
+    props:true
+  },
+  // redirect
+  {
+    path:'/all-jobs',
+    redirect:'/jobs'
+  },
+  // catch error 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'Error',
+    component: Error
   }
 ]
 
---
+++ vue-jobs/src/views/Jobs/Jobs.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+      <router-link :to="{name: 'JobDetails', params:{id: job.id}}">
+          <h2>{{job.title}}</h2>
+      </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            jobs: [
+                {title:'UX designer', id:1, details:'sajsad'},
+                {title:'Web Developer', id:2, details:'ejfajsad'},
+                {title:'Vue Developer', id:3, details:'ghrtjsad'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
